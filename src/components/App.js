13439891ed6ad0dc346fff7791db9e654af498cd
Kyle Parry <kyle.parry.303@gmail.com>
2020-12-09 14:38:20 -0700

    Basic CSS layout complete stage Level 0

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import CSS from '../main.css';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
   render(){
     return(
-    <div>
-      Starter App component - hello there
+    <div id='page'>
+      <h1>Kyle's Movie List</h1>
+        <div>
+          <a>Mean Girls</a>
+          <a>Hackers</a>
+          <a>The Grey</a>
+          <a>Sunshine</a>
+          <a>Ex Machina</a>
+        </div>
     </div>
   )}
 }
